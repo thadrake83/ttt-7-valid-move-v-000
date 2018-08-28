c603a1ef34affdbaa04cde0334952fba99cbd3d2
DRAKE RENTROP <drakerentrop@gmail.com>
2018-08-28 18:21:47 +0000

    Done.

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,14 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+
+
+
+def position_taken?(board, index)
+ board[index] !=  " " && board[index] != "" && board[index] != nil
+end
+  
+  def valid_move?(index, board)
+    (0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8) && ! position_taken?(board, index)
+    
+  end
