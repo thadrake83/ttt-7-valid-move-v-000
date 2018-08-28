bf7cd9e5eca64a93f0b484178b584069dc314101
DRAKE RENTROP <drakerentrop@gmail.com>
2018-08-28 18:28:10 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(board, index)
  board[index] !=  " " && board[index] != "" && board[index] != nil
 end
   
-  def valid_move?(index, board)
+  def valid_move?(board, index)
     #(0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8) 
     index.between?(0, 8) && !position_taken?(board, index)
     
